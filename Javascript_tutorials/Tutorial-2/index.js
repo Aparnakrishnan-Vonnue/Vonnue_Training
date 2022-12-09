d6bec3e995e95b2b043cc9f8a48bcf5c29a41753
Aparna <aparna.k@vonnue.com>
2022-12-09 17:56:39 +0530

    feat: watch and practise another video tutorial of javascript

---
+++ Javascript_tutorials/Tutorial-2/index.js
@@ -0,0 +1,17 @@
+let selectedColors = [['red', 'blue']];
+selectedColors[2] =1;
+console.log(selectedColors.length);
+
+// function greet(name) {
+//     console.log('Hello' + name)
+// }
+// greet('John');
+// greet('Mary');
+
+function square(number) {
+  return  number * number;
+}
+ square(2);
+ let number = square (2);
+ console.log(number);
+  
