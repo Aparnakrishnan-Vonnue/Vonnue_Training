0ab35790f0afbae600dec9a67e938c17669abcfd
Aparna <aparna.k@vonnue.com>
2022-12-10 20:58:40 +0530

    feat: task modified in main branch

---
+++ Javascript_tutorials/task/task3.js
@@ -0,0 +1,9 @@
+//How to make tea with probabilities:
+//with sugar and with milk
+//with sugar and without milk
+//without sugar and with milk 
+//without sugar and without milk
+
+let itemToBeMade = "tea"
+var ingredient1 = "sugar"
+var ingredient2 = "milk"
