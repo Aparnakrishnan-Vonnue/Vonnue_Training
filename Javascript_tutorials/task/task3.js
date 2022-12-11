31bbdddc33e2e737d1c3596dac8c5acb6fc69e79
Aparna <aparna.k@vonnue.com>
2022-12-11 13:33:53 +0530

    feat: javascript tutorial basic progra practisem

--- Javascript_tutorials/task/task3.js
+++ Javascript_tutorials/task/task3.js
@@ -4,3 +4,15 @@
 //without sugar and with milk 
 //without sugar and without milk
 
+let tea1 = a + c + d
+let tea2 = b + c + d
+let tea3 = c + d
+
+var a = sugar
+var b = milk
+var c = water
+var d = teaPowder
+var tea = a + b + c + d
+if (a + b + c + d === true) {
+    console.log()
+}
