8e6934ec81c73bbac84ed031ee67219edf5468d7
Aparna <aparna.k@vonnue.com>
2022-12-31 20:00:08 +0530

    feat:Udemy Javascript tutorial chapter 277 in progress

--- Javascript_tutorials/udemy-course/scriptt.js
+++ Javascript_tutorials/udemy-course/scriptt.js
@@ -1,6 +1,97 @@
 //importing module
-import './shoppingCart.js';
-console.log('importing module');
-// console.log(shippingCost)
+// import './shoppingCart.js';
+// import { addTOCart, totalPrice as Price, tq } from './shoppingCart.js';
+//addToCart('bread', 5);
+//console.log(price, tq)
+//  console.log('importing module');
 
-addTOCart('bread', 5);
+//  // console.log(shippingCost)
+// //  import * as ShoppingCart from './shoppingCart.js';
+// //  ShoppingCart.addToCart('bread', 5); 
+// //  console.log(ShoppingCart, totalPrice);
+
+// // import add, {addToCart, totalPrice as price, tq} from './shoppingCart.js'
+// // console.log(price);
+
+// import add, {cart} from './shoppingCart.js';
+// add('pizza',2);
+// add('bread',5);
+// add('apples',4);
+
+// console.log(cart);
+
+// // console.log('Start fetching');
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
+// // console.log('Something')
+
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+//   console.log(data);
+
+//   return {title: data.at(-1).title,text: data.at(-1).body }
+// }
+// const lastPost =  getLastPost();
+// console.log(lastPost);
+// //Not very clean
+// //lastPost.then(last => console.log(last));
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+// (function() {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addTOCart = function (product, quantity) {
+//     cart.push({product, quantity});
+//     console.log(`${quantity} ${product} added to cart`)
+//   };
+
+//   const orderStock = function(product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   }
+//   return {
+//     addTOCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   }
+// })();
+
+// ShoppingCart2.addTOCart('apple', 4);
+// ShoppingCart2.addTOCart('Pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
+
+//Exxport
+// export.addTOCart = function (product, quantity) {
+//   cart.push({product, quantity});
+//   console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`)
+// };
+
+// //Import
+// const {addToCart} = required('./shoppingCart.js')
+
+//COMMAND LINE
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+const state = {
+  cart: [
+    {product: 'bread', quantity: 5},
+    {product: 'pizza', quantity: 5}
+  ], 
+  user : {loggedIn:true},
+};
+const stateClone = Object.assign({},state);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
--- Javascript_tutorials/udemy-course/shoppingCart.js
+++ Javascript_tutorials/udemy-course/shoppingCart.js
@@ -1,11 +1,25 @@
 //Exporting module
 console.log('Exporting module');
 
+//Blocking code
+console.log('Start fetching users');
+await fetch('https://jsonplaceholder.typicode.com/posts');
+console.log('Finish fetching users');
+
 const shippingCost = 10;
 const cart = [];
-if (true) {
+
  export const addTOCart = function (product, quantity) {
     cart.push({product, quantity});
     console.log(`${quantity} ${product} added to cart`);
   }
-}
+
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  export {totalPrice, totalQuantity as tq};
+
+  export default function (product, quantity) {
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart`)
+  }
